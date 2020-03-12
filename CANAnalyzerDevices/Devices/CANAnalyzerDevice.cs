52aced5684d672c35bdf47554382dc7d9e366cb4
Firsau Sergei <sergeyfk05@gmail.com>
2020-03-12 11:48:57 +0300

    init

---
+++ CANAnalyzerDevices/Devices/CANAnalyzerDevice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Management;
+
+namespace CANAnalyzerDevices.Devices
+{
+    public class CANAnalyzerDevice
+    {
+        public CANAnalyzerDevice()
+        {
+            string query = "SELECT * FROM Win32_SerialPort";
+
+            string[] ModemObjects = new string[250];
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            byte k = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+
+
+                ModemObjects[k] = obj["Name"].ToString() + "(" + obj["AttachedTo"].ToString() + ")";
+                k += 1;
+
+            }
+
+            return; 
+        }
+    }
+}
---
+++ CANAnalyzerDevices/Devices/IDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace CANAnalyzerDevices.Devices
+{
+    public interface IDevice
+    {
+        bool IsSupportWorkWith(ManagementObject obj);
+    }
+}
