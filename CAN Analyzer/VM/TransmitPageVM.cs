9db84eb8a2d966d726133c555d52b8b31ff28014
Firsau Sergei <sergeyfk05@gmail.com>
2020-03-13 18:35:05 +0300

    1231

---
+++ CAN Analyzer/VM/TransmitPageVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicResource;
+using CANAnalyzer.Models;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace CANAnalyzer.VM
+{
+    public class TransmitPageVM : BaseVM
+    {
+
+        private ICommand _transmitToComboBoxSelected;
+        public ICommand TransmitToComboBoxSelected
+        {
+            get
+            {
+                if (_transmitToComboBoxSelected == null)
+                    _transmitToComboBoxSelected = new RelayCommandWithParameter<ComboBox>(this.TransmitToComboBoxSelected_Execute);
+
+                return _transmitToComboBoxSelected;
+            }
+        }
+        private void TransmitToComboBoxSelected_Execute(ComboBox arg)
+        {
+            arg.SelectedIndex = 0;
+        }
+    }
+}
