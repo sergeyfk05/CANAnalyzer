9a1036beeb498c2d5c227bff37eb97c5d4b388ad
Firsau Sergei <sergeyfk05@gmail.com>
2020-03-24 23:36:36 +0300

    add doc

--- CANAnalyzerDevices/Devices/IDevice.cs
+++ CANAnalyzerDevices/Devices/IDevice.cs
@@ -12,12 +12,29 @@ namespace CANAnalyzerDevices.Devices
 {
     public interface IDevice
     {
+        /// <summary>
+        /// Try connect to hardware device.
+        /// </summary>
         void Connect();
+
+        /// <summary>
+        /// Disconnect hardware device.
+        /// </summary>
         void Disconnect();
+
+        /// <summary>
+        /// Available CAN channels.
+        /// </summary>
         IEnumerable<IChannel> Channels { get; }
 
+        /// <summary>
+        /// Channels count
+        /// </summary>
         int ChannelCount { get; }
 
+        /// <summary>
+        /// Hardware device status.
+        /// </summary>
         bool IsConnected { get; }
     }
 }
