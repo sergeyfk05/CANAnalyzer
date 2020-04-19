2bebc72aeb023a41df75f3d69569e9718d42732b
Firsau Sergei <sergeyfk05@gmail.com>
2020-04-19 14:07:58 +0300

    add some comments

--- CAN Analyzer/Models/ChannelsProxy/IChannelProxy.cs
+++ CAN Analyzer/Models/ChannelsProxy/IChannelProxy.cs
@@ -13,12 +13,25 @@ namespace CANAnalyzer.Models.ChannelsProxy
 {
     public interface IChannelProxy : IChannel, IDisposable
     {
+        /// <summary>
+        /// target file
+        /// </summary>
         string Path { get; }
+        /// <summary>
+        /// owner channel
+        /// </summary>
         IChannel Channel { get; }
 
+        /// <summary>
+        /// human friendly name
+        /// </summary>
         string Name { get; set; }
         event EventHandler NameChanged;
 
+        /// <summary>
+        /// set another owner channel
+        /// </summary>
+        /// <param name="newCH"></param>
         void SetChannel(IChannel newCH);
     }
 }
