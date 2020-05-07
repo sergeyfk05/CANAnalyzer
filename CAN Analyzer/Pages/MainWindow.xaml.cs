f872478140cd5bd847b07cebaf145da43f2b2068
Firsau Sergei <sergeyfk05@gmail.com>
2020-05-07 13:12:08 +0300

    relocate MainWindow

--- CAN Analyzer/Pages/MainWindow.xaml.cs
+++ CAN Analyzer/Pages/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CANAnalyzer
+namespace CANAnalyzer.Pages
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
