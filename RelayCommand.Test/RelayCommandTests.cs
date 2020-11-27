af0a1de4aaf53e488e842ed9f6ea89a553fe0796
sergeyfk05 <sergeyfk05@gmail.com>
2020-11-27 12:24:41 +0300

    migrate to .net5

---
+++ RelayCommand.Test/RelayCommandTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xunit;
+
+namespace RelayCommands.Test
+{
+    public class RelayCommandTests
+    {
+
+        public void ExecuteTest()
+        {
+            //RelayCommand command = new RelayCommand(() => Assert.)
+        }
+    }
+}
